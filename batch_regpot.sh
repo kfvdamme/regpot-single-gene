edbe4e924c48d8b8f9e0160a9ff48611d8ee0f58
pieterdb60 <115018893+pieterdb60@users.noreply.github.com>
2024-03-20 09:01:26 +0100

    Add files via upload

--- batch_regpot.sh
+++ batch_regpot.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-while IFS=, read -r gene_symbol species
+# Remove the file if it exists
+if [ -f rp_score_all_TF.csv ]; then
+    rm rp_score_all_TF.csv
+fi
+
+while IFS=, read -r gene_symbol species half_decay
 do
-    ./regpot_single_gene_hg38_mm10.py "$gene_symbol" "$species" 10kb
-done < genes_oi.txt
+    ./regpot_single_gene_hg38_mm10.py "$gene_symbol" "$species" "$half_decay"
+    done < genes_oi.txt
